cc80249382820874455f14da48f27b944045cc7e
alex <tech@poloniculmov.com>
2019-12-17 12:02:45 +0200

    4 b

--- 2019/four/main.go
+++ 2019/four/main.go
@@ -1,12 +1,15 @@
 package main
 
-import "strconv"
+import (
+	"regexp"
+	"strconv"
+)
 
 func main() {
 	count := 0
 	for i := 372304; i <= 847060; i++ {
 		if checkPassword(strconv.Itoa(i)) {
-			println(i)
+			//println(i)
 			count++
 		}
 	}
@@ -14,16 +17,17 @@ func main() {
 	//println(checkPassword("233789"))
 }
 
+//
 func checkPassword(password string) bool {
 	if len(password) != 6 {
 		return false
 	}
+
 	adjacentLetters, decreasing := false, false
+	re := regexp.MustCompile(`(([^1]|^)1{2}([^1]|$))|(([^2]|^)2{2}([^2]|$))|(([^3]|^)3{2}([^3]|$))|(([^4]|^)4{2}([^4]|$))|(([^5]|^)5{2}([^5]|$))|(([^6]|^)6{2}([^6]|$))|(([^7]|^)7{2}([^7]|$))|(([^8]|^)8{2}([^8]|$))|(([^9]|^)9{2}([^9]|$))|(([^0]|^)0{2}([^0]|$))`)
+	adjacentLetters = re.MatchString(password)
 	letter := password[0]
 	for i := 1; i < len(password); i++ {
-		if letter == password[i] {
-			adjacentLetters = true
-		}
 		if letter > password[i] {
 			decreasing = true
 		}
